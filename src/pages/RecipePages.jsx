dedb27a74853d6b6f7e57818be80491c67a84f20
Frederiek <fbrongers@gmail.com>
2024-04-16 15:51:52 +0200

    Remove RecipePages

--- src/pages/RecipePages.jsx
+++
@@ -1,9 +0,0 @@
-import { RecipePage } from "./RecipePage";
-
-export const RecipePages = ({recipes, onClick}) => {
-    return (
-        recipes.map((recipe) => (
-            <RecipePage key={recipe.hit} recipe={recipe} onClick={onClick}></RecipePage>
-        ))
-    )
-}
